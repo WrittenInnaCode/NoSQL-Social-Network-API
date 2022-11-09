4f52f5caf25764a005fc82d5611f730654fc9425
WrittenInnaCode <writteninnacode@gmail.com>
2022-11-08 20:16:09 -0800

    created models and added Dayjs for timestamp

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+const { Schema, Types } = require('mongoose');
+const dayjs = require('dayjs');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dayjs(timestamp).format('MMM DD, YYYY [at] hh:mm a'),
+        }
+    },
+    {
+        toJSON: {
+          getters: true,
+        },
+        id: false,
+      }
+)
+
+
+module.exports = ReactionSchema;
